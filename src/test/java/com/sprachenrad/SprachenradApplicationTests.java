c65c9de79107e353112d687ed1ad79643aa437bd
Esteban Boucher <es.boucher@protonmail.com>
2023-07-31 01:53:37 +0200

    Added Spring Boot project

---
+++ src/test/java/com/sprachenrad/SprachenradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprachenrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprachenradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
