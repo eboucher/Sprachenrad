b07ca6d915f7dbe987fec588df081d95eb779cea
Esteban Boucher <es.boucher@protonmail.com>
2023-07-31 18:24:21 +0200

    Add HomeController and static files, update pom.xml and .gitignore

--- src/main/java/com/sprachenrad/SprachenradApplication.java
+++ src/main/java/com/sprachenrad/SprachenradApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SprachenradApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SprachenradApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SprachenradApplication.class, args);
+  }
 
 }
