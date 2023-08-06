e5059cb5c01c91486a18e67d726d23b021549ba7
Esteban Boucher <es.boucher@protonmail.com>
2023-08-07 00:18:05 +0200

    SPRAC-93 Add PostgreSQL initialization script

---
+++ db-init-scripts/init.sql
@@ -0,0 +1,9 @@
+-- db-init-scripts/init.sql
+
+CREATE SCHEMA IF NOT EXISTS jwt_security;
+
+-- Add more SQL statements to create tables and seed data
+-- Example:
+-- CREATE TABLE IF NOT EXISTS jwt_security.users (
+--    ...
+-- );
