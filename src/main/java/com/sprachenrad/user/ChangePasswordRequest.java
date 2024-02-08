1e199594b337c79d1e11d9cb9a997c42b5e771f1
Esteban Boucher <es.boucher@protonmail.com>
2024-02-08 16:29:32 +0100

    SPRAC-92 Added User controller & service

--- src/main/java/com/sprachenrad/user/ChangePasswordRequest.java
+++ src/main/java/com/sprachenrad/user/ChangePasswordRequest.java
@@ -1,4 +1,4 @@
-package com.alibou.security.user;
+package com.sprachenrad.user;
 
 import lombok.Builder;
 import lombok.Getter;
--- src/main/java/com/sprachenrad/user/UserController.java
+++ src/main/java/com/sprachenrad/user/UserController.java
@@ -1,4 +1,4 @@
-package com.alibou.security.user;
+package com.sprachenrad.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
